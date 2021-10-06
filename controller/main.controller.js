d2ff9fc33f147755ea7ccbf18533c5a39756de05
uzair-ali10 <the.uzairali10@gmail.com>
2021-10-06 21:13:59 +0530

    Initial Commit

---
+++ controller/main.controller.js
@@ -0,0 +1,3 @@
+exports.mainFunction = async (req, res, next) => {
+    return res.status(200).json({Message: "Welcome to Express Boilerplate", "Github Repository": "https://github.com/uzair-ali10/express-boilerplate"});
+}
